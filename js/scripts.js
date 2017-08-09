2d84dfbb589003e428a36d1a358b04d19cc87bdf
Daniel Kiss and Dawson Mortenson <daniel@>
2017-08-09 14:49:38 -0700

    add multiple and divide to calculator

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,4 @@
+// Business (or back-end) logic:
 var add = function(num1, num2) {
 	return num1 + num2;
 }
@@ -11,7 +12,43 @@ var divide = function(num1, num2) {
 	return num1 / num2;
 }
 
-var one_num = parseInt(prompt("Enter a number: "));
-var two_num = parseInt(prompt("Enter an additional number: "));
+//Everything below this line is user interface (or front-end) logic:
 
-alert(divide(one_num, two_num));
+$(document).ready(function() {
+	$("form#add").submit(function(event) {
+	 	event.preventDefault();
+
+		var number1 = parseInt($("#add1").val());
+  	var number2 = parseInt($("#add2").val());
+  	var result = add(number1, number2);
+
+		$("#output").text(result);
+	});
+	$("form#subtract").submit(function(event) {
+	 	event.preventDefault();
+
+		var number1 = parseInt($("#sub1").val());
+  	var number2 = parseInt($("#sub2").val());
+  	var result = subtract(number1, number2);
+
+		$("#output").text(result);
+	});
+	$("form#multiply").submit(function(event) {
+	 	event.preventDefault();
+
+		var number1 = parseInt($("#multi1").val());
+  	var number2 = parseInt($("#multi2").val());
+  	var result = multiply(number1, number2);
+
+		$("#output").text(result);
+	});
+	$("form#divide").submit(function(event) {
+	 	event.preventDefault();
+
+		var number1 = parseInt($("#divide1").val());
+  	var number2 = parseInt($("#divide2").val());
+  	var result = divide(number1, number2);
+
+		$("#output").text(result);
+	});
+});
